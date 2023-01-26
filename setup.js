672d74be1b4781e52b0da444125716d6ddde8804
SL-FassettLab <123701193+SL-FassettLab@users.noreply.github.com>
2023-01-26 14:57:32 -0800

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,10 +1,17 @@
-# SL-FassetLab.github.io
-function init() {
-  Tabletop.init( {
-    key: '1YfU6MiCcJvIkKFhukfpL5Mwosh41Cv45MQZdBwbQgec',
-    simpleSheet: true }
-  ).then(function(data, tabletop) { 
-    console.log(data)
-  })
-}
-window.addEventListener('DOMContentLoaded', init)
+<script src='https://cdnjs.cloudflare.com/ajax/libs/tabletop.js/1.5.1/tabletop.min.js'></script>
+<script type='text/javascript'>    
+  var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1YfU6MiCcJvIkKFhukfpL5Mwosh41Cv45MQZdBwbQgec/edit?usp=sharing';
+
+  function init() {
+    Tabletop.init( { key: publicSpreadsheetUrl,
+                     callback: showInfo,
+                     simpleSheet: true } )
+  }
+
+  function showInfo(data, tabletop) {
+    alert('Successfully processed!')
+    console.log(data);
+  }
+
+  window.addEventListener('DOMContentLoaded', init)
+</script>
